8f5ffcadc94f67a343aa3bb1023f4616a952841d
kaegan <kkiefer4@student.umgc.edu>
2024-06-10 11:17:37 +0900

    Submission 1

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int number = (int) (numberToBeRounded + .5);
+        System.out.println(number);
     }
 }
